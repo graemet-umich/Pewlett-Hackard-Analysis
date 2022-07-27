d65c36e8f1a8bc3e250ba2296b9e5e11e8d79c16
Graeme Tank <graeme.tank@gmail.com>
2022-07-27 13:57:42 -0400

    employee information

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -132,3 +132,27 @@ ORDER BY de.dept_no;
 
 SELECT * FROM num_ret_elig_by_dept_no;
 
+
+
+-- 7.3.5 Create Additional Lists
+
+---- List 1: Employee Information
+
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+SELECT e.emp_no,
+       e.first_name,
+       e.last_name,
+       e.gender,
+       s.salary,
+       de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+   AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+   AND (de.to_date = '9999-01-01');
