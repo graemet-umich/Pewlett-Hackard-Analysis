92ab18982714603b73aab04addef9ee455ac0011
Graeme Tank <graeme.tank@gmail.com>
2022-07-27 11:59:24 -0400

    current employees eligible to retire (using joins)

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -55,3 +55,55 @@ AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 -- Check the table
 SELECT * FROM retirement_info;
 
+
+-- Joining departments and dept_manager tables
+-- shows all employees who ever managed each department
+SELECT departments.dept_name,
+     dept_manager.emp_no,
+     dept_manager.from_date,
+     dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+       retirement_info.first_name,
+       retirement_info.last_name,
+       dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+-- Joining retirement_info and dept_emp tables
+-- redux: use aliases
+SELECT ri.emp_no,
+       ri.first_name,
+       ri.last_name,
+       de.to_date
+FROM retirement_info AS ri
+LEFT JOIN dept_emp AS de
+ON ri.emp_no = de.emp_no;
+
+-- Joining departments and dept_manager tables
+-- redux: use aliases
+SELECT d.dept_name,
+       dm.emp_no,
+       dm.from_date,
+       dm.to_date
+FROM departments AS d
+INNER JOIN dept_manager AS dm
+ON d.dept_no = dm.dept_no;
+
+-- Current employees eligible for retirement
+SELECT ri.emp_no,
+       ri.first_name,
+       ri.last_name,
+       de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+SELECT * FROM current_emp;
