8820a2c3173087ffb3f58d4d3f4041974c8c4495
Graeme Tank <graeme.tank@gmail.com>
2022-07-27 14:56:52 -0400

    department retirees

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -194,3 +194,18 @@ INNER JOIN departments AS d
 INNER JOIN current_emp AS ce
     ON (dm.emp_no = ce.emp_no);
  
+-- List 3: Department Retirees
+
+-- Multiple entries per employee result from inclusion of employees'
+-- old jobs
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+-- INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
